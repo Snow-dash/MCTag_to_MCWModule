7034554e6852583f95ce390c20dcd13121dbb1e1
Snowdash <sunnywarmsea@gmail.com>
2022-10-23 11:23:44 +0800

    first

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+import tool
+
+originPath = sys.path[0]
+
+sys.path.append(originPath + "\\slpp-23-master")
+
+os.chdir(originPath)
+
+from slpp import slpp as lua
+
+
+def gen_tag(dic):
+    out = {}
+    re = False
+    for tp in dic:
+        for tag in dic[tp]:
+            if tp not in out:
+                out[tp] = {}
+            if tag not in out[tp]:
+                out[tp][tag] = []
+            for item in dic[tp][tag]:
+                if "#" in item:
+                    for i in dic[tp][item[11:]]:
+                        if i.startswith("minecraft:"):
+                            out[tp][tag].append(i[10:])
+                        else:
+                            out[tp][tag].append(i)
+                    re = True
+                else:
+                    if item.startswith("minecraft:"):
+                        out[tp][tag].append(item[10:])
+                    else:
+                        out[tp][tag].append(item)
+    if re:
+        return gen_tag(out)
+    return out
+
+
+def gen_id(dic):
+    out = {}
+    for tp in dic:
+        if tp not in out:
+            out[tp] = {}
+        for tag in dic[tp]:
+            for i in dic[tp][tag]:
+                if i not in out[tp]:
+                    out[tp][i] = []
+                out[tp][i].append(tag)
+    out = tool.sort(out)
+    return out
+
+
+if __name__ == "__main__":
+    result = {}
+    if len(sys.argv) == 1:
+        print("usage: main.py [path]")
+    else:
+        tagdic = {}
+        for pa in sys.argv[1:]:
+            tagdic = tool.load(pa, tagdic)
+
+        tagdic = tool.sort(tagdic)
+        result["tag_ori"] = tagdic
+
+        result["tag"] = gen_tag(tagdic)
+
+        result["ID"] = gen_id(result["tag"])
+        with open("out.txt", "w") as fiout:
+            fiout.write(lua.encode(result))
---
+++ tool.py
@@ -0,0 +1,60 @@
+import sys, json, os
+
+
+def upper_dirs(path):
+    return str.join("\\",str.split(path, "\\")[0:-1])
+
+def lastone(path):
+    return  str.split(path,"\\")[-1]
+
+def lasttwo(path):
+    return str.join("\\",str.split(path,"\\")[-2:-1])
+
+def backtwo(path):
+    return str.split(path,"\\")[-2]
+
+
+def load(path,out=None):  # \data\minecraft\tags
+    if out is None:
+        out = {}
+    tag_path = path + "\\data\\minecraft\\tags"
+    tagpath_li = []
+    for root, dirs, files in os.walk(tag_path, topdown=True):
+        #print(root, dirs, files)
+        # print("\n")
+
+        if len(files) != 0:
+            #print(root, dirs, files)
+            #print(upper_dirs(root))
+            if upper_dirs(root) in tagpath_li:
+                for fi in files:
+                    with open(root + "\\" + fi) as onejsonfi:
+                        context = json.loads(onejsonfi.read())
+                        if backtwo(root) not in out:
+                            out[backtwo(root)] = {}
+                        if lastone(root) + "/" + fi[0:-5] not in out[backtwo(root)]:
+                            out[backtwo(root)][lastone(root) + "/" + fi[0:-5]] = []
+                        for item in context["values"]:
+                            out[backtwo(root)][lastone(root) + "/" + fi[0:-5]].append(item)
+            else:
+                for fi in files:
+                    with open(root + "\\" + fi) as onejsonfi:
+                        context = json.loads(onejsonfi.read())
+                        if lastone(root) not in out:
+                            out[lastone(root)] = {}
+                        if fi[0:-5] not in out[lastone(root)]:
+                            out[lastone(root)][fi[0:-5]] = []
+                        for item in context["values"]:
+                            out[lastone(root)][fi[0:-5]].append(item)
+
+
+            if root not in tagpath_li:
+                tagpath_li.append(root)
+    return out
+
+def sort(dic):
+    for tp in dic:
+        for tag in dic[tp]:
+            dic[tp][tag] = sorted(dic[tp][tag])
+    return dic
+
